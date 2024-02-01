ae60f521939f8240914606ce311abade87d3e32b
AADESHRN <157205248+AADESHRN@users.noreply.github.com>
2024-02-01 10:15:32 +0530

    Sorry App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h5>Ankita <div className="heartbeating-text">💗</div> Aadesh</h5>
-      <h2>Jai Siya Ram Cutomuto! 🚩❣️</h2>
+      <h2>Sorry Gugguli! 🥺❣️</h2>
     </>
   )
 }
