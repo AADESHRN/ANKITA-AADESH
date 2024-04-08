450abee08c0f2ecd8a83d665e69f81be949cbcf5
aadesh <aadeshnirfarake.yt@gmail.com>
2024-04-09 00:31:59 +0530

    added text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,135 @@
-import './App.css'
+import './App.css';
+import './tailwind.css';
 
 function App() {
 
   return (
-    <>
+    <div className=''>
       <h5>Ankita <div className="heartbeating-text">💗</div> Aadesh</h5>
-      <h2>Good Afternoon CutiePie!!</h2>
-    </>
+      <p>
+      I am not comfertable in writing that much in Hindi or Marathi. So writing in English and little hindi.
+I can write a whole book about it but trying to keep everything in short.
+I never thought that someday I would have to right this. But this is our last ray of hope.
+Please read this not as Ankita's older brother but as My or Ankita's best friend.
+As Ankita's brother, It may not sound good to you but I too have a sister and we live in 21st century so please read it with open mind. 
+I hope you will understand from my and ankita's perspective as a friend.
+Writing this with heavy heart at 2 in night.
+
+You already know me and Ankita are classmates from 11th & 12th.
+But there is much more you should and need to know.
+I am not sure if I would ever get the guts to send you this message, But if I do, just know that I am in deep love & pain and can't handle it.
+
+So the story starts long back during 11th 12th college itself. 
+I used to like Ankita (As Ankita's brother, It may not sound good to you but hope you will understand from my and ankita's perspective)
+But in those days I used to be a nerd who always used to focus on studies. So I never really talked with her about this. 
+Later in the story I got to know that she was also in the asme boat. She liked me but never told me. 
+As she said she tried to talk to me on our final practical. But she couldn't because of anxiety & shyness.
+
+After that day we never really talked or stayed in touch. I was topper in 12th science in the collage & my classes. 
+
+After all this I moved to Pune for engineering in one of the top colleges in Pune i.e. MIT, Kothrud.
+I did the college sincearly & in my final year after multiple job offers I joined Persistent system as Software Engineer in August 2020.
+Days passed and I started using social media a little. Connected with old friends. Connected with Ankita.
+Our memories got refreshed, we started talking, shared numbers, I even started using snapchat because of her.
+
+We have been talking day and night with each other since more that past 1.5+ years now. Both of us know everything about each others life.
+I know everything about your family members.
+
+Mumma ka previous name - Sunita Madharlele - she is from nagar - Your Nani ji was marathi, she did intercase marrige, which didn't go very well in their old age and she left the home and never came back 
+Mummi ki birthdate - 10th March
+
+Papa - Rajkumar Choukse - from MP - have multiple 8-9 sisters(Bua) and one big brother(bade papa) - 
+I know about Papa's past struggle - how they came to nagar - How you guys used to live in saras nagar - with mama ji - 
+How papa found his way through all the ups and downs and set up the business of fruits trading - How he goes to supa and different markets - His holidays on Thursdays and Saturdays
+Papa birthdate - 28th Oct
+
+Saurabh Bhiyya (You) - Ankita & Me both are really proud of you, How you have acheived your career heights - 
+she always tells me about your exhebitions or events or the company parties - I always get all your pics as your family member as soon as you send it to her
+I remember your Dubai exhebition.. I have all the pics of you in dubai.. WIth Burj Khalifa.. Your event..
+Then about goa exhebition where PM modi was gonna come.. The suit you bought few days before going to goa.. Which was amazing btw..
+Also I get the updates everytime you come home. Snap of she doing your champi. You guys eating panipuri with family. 
+You last visited home around padwa.. You went to Suzuki for getting new bike, you went to grab vada pao and lassi from bazaar.. I always get updates about All the lovely moments.
+Bhiyya Birthdate - 19th March
+
+Ankita - I can never finish writing how much I know about her & how much I understand her. She has told me everything about her. What all thing she did after 12th. 
+The Engineering, her intrests hobbies, And also about Aman. How he came & destroyed her life. How you guys went to his family marrige. and many thing which none of us would like to talk about.
+After the breakup how she felt and how everyone treated her. Every small thing.
+But forget the sad part. Her tries for the government pwd exam few months back. her Salesforce course in pune.
+Not only this, But all her dresses, favourite things, make up items, 
+She has sent me soo many pictures of her that I have more pics of her than my own in my phone gallary now.
+She taught me what soulmate is - she told me how me and she are soulmates - And she regret why I didn't found her before Aman.
+During all this time we spend - we have seen many good and bad things togather - 
+we fough with each other multiple times and always came back - cared and loved each other
+But we never met. We have maintained boundries till now. We have talked for hours on calls, messages, 
+Also face to face we have talked is many times with she in your gallry and I standing in front of her in the hanuman temple.
+Also one time I surprised her with a gift box directly in front of your house. She was shy and nervous. These are our only face to face encounters and we have never met outside anywhere till now.
+And neither of us have forced anything on each other ever.
+
+So many things has happened in all this time. And recently things started getting so serious.
+That's why I dared to meet you in that marrige that day. All I wanted to do is build a friendship with you. But I never got much response from you later..
+Mumma already know everything about me - As she and my bua were classmates.
+I have introduced Ankita to few of my family members. She knows my chachi very well now and she talks with her frequently on calls & messages.
+They have discussed about our marrige and how we will face blockers and how we have to convince everyone.
+She said that mummy will understand and and she is just scared of papa and you.
+She was also concerned that because of her past and Nani's incident, you guys won't allow her to do love marrige anymore and you guys will shout at her or mistreat her.
+She is also scared that because of her past or your two experiences with marathi girls even you won't support her as friend & brother. 
+She has the past trauma from which she is not healed completely and scared of her own family.
+But still, for love she tried to talk to mummy on Thursday 4th April. 
+Mummy's reaction was quite obvious and firstly she talked about the cast issues, then gave examples of failed marriges, 
+they talked for 3 hours and mummy demotived Ankita & told her that if she told it to papa you guys will get her married in 2 months with anyone.
+So basically she is in very bad situation where she has to think about samaj, family pressure, and us.
+And she doesn't wanna make you guys feel bad.  So she is ready to get merried to whoever unknown guy you guys will get for her. 
+She is basically killing herself & her emotions due to past & due to fear of you guys.
+But I think there is only one in your family who is educated and mature enough to understand us and it is You Bhiyya.
+
+I know while reading this you might be feeling mixed emotions some anger, some care, some understanding
+But Please try to understand everything.
+Your parents will anyways get Ankita married in next 1 year with some random stranger whose past & future you will have no clue about.
+And she is very innocent that for your happiness she will get married too. And she is ready to do all the sacrifises for your family. 
+
+Let me introduce a bit about myself too
+
+I am Aadesh Nirfarake, I have a home in maliwada 
+(I know it might not sound good because of the rowdy status this place has 
+but I have always been an exception because Never had any chapri friends and I have always worked hard & acheived my dreams which you would get to know below)
+(I was schooleed in Bhausaheb Firodiya till 10th 
+- Then for 12th I went to MM Ware just because I could skip attending college lectures and do the study - 
+In 12th I came first in that college and also in my classes(If you know Gawande classes) in both boards & CET
+- There were news and banners all over the city)
+
+Later I went to MIT College of Engineering, Kothrud - One of the top colleges in maharashtra for Computer Engineering.
+I did my batcholers sincerely and finished it with very good score and multiple job offers in hand in last year.
+I joined persistent Systems in august 2020.
+Did my first switch in 2022 June, where with more than 200% hike I joied the citi group - one of the largest investment bank in USA
+There as well within just one year I got promoted to senior role due to my performance with 42% hike.
+
+I started getting 6 figures monthly income since I was 22 & Currently at the age of 24 I am at around 24 LPA CTC.
+
+With earnings, finantial knowledge, savings & investments are also important. 
+I invest most of my money in equities & also a little in real estate (I am building a house in savedi on my fathers land).
+I have mutual fund portfolio of around 18 lakhs and around 3.5 lakhs worth of stocks up till now. And a Monthly SIP of 50k+ which I am targetting to make 1 lakh+ per month before I complete 25 age.
+Other than this I only like to spend money on Bike (I have yezdi scrambler which was around 3 lakhs with accessories) & Travel (Have travelled many parts of india)
+
+This was about me.
+
+My family :
+
+My father works in L&T (now schneider elextrical) in quality department 
+My mother is housewife
+My younger sister is also a Computer Engineer from PICT Pune (2023) and she has joined a company right after college with 18 LPA CTC
+My uncle is a Plant Head in Kirlosker Pneumatics Pune
+
+Basically the motive of telling all this is 
+I just want to assure you that I am a nice, smart & diciplined guy who goes to gym 4 days a week, who work, 
+who is working on building parallel businesses, who basically know his future goals and plans,
+who have only loved one girl in him lifetime and that's Ankita & 
+Who have a very cool family which can support & love Ankita for lifetime and all of us can be Ankitas peaceful home for life. 
+    
+
+
+Because of my good score in boards & CET, I got lots of fame. My name was on all newspapers & hordings of my classes all over the city.
+      </p>
+    </div>
   )
 }
 
