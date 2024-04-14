b210229386d3b30e3c92f16392bc96ac202edbbf
AADESHRN <157205248+AADESHRN@users.noreply.github.com>
2024-04-14 16:17:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ function App() {
 
   return (
     <div className=''>
-      <h5>Ankita <div className="heartbeating-text">💗</div> Aadesh</h5>
+      <h5>Ankita <div className="heartbeating-text">💗</div> Aadesh</h5><br/><br/>
       <p>
-      I'm not very comfortable writing in Hindi or Marathi, so I'll write in English with a bit of Hindi.
-I can write a write a whole book about it about this, but I'll try to keep it short. 
+      I'm not very comfortable writing in Hindi or Marathi, so I'm trying to write in English with a bit of Hindi.
+I can write a write a whole book about it about this, but I'll try to keep it short.
 I never imagined I would have to write this, but it's our last hope.
 Please read this not as Ankita's older brother, but as best friend of me & Ankita. 
 I too have a sister so I know that as Ankita's brother, you might not like it at first, but we're in the 21st century, so please read with an open mind. 
-I hope you can understand from my and Ankita's perspective. Writing this with a heavy heart at 2 in the night.
+I hope you can understand from my and Ankita's perspective. 
+Writing this with a heavy heart at 2 in the night.
 
 You already know Ankita and I were classmates in 11th and 12th. But I think this is the time, there's much more you should know. 
 I'm not sure if I'll ever have the courage to send you this message, but if I do, know that I'm deeply in love and in fear of losing, and I can't handle it.
+
 So, the story goes back to our college days. I liked Ankita then (as her brother, this might not sound good to you, but I hope you understand). 
 But I was always focused on studies and never talked to her about it. 
 Later in the story, I found out she felt the same but couldn't tell me due to anxiety and shyness.
@@ -27,10 +29,10 @@ As I started using social media, I reconnected with Ankita. We started talking a
 We've been talking day and night for over 1.5 years now. We know everything about each other's lives, including each others families. 
 I know everything about your family members
 
-Mumma - previous name 'Sunita Madharlele', she is from nagar, Your Nani ji was marathi, she did intercase marrige, which didn't go very well in their old age and she left the home and never came back 
+Mumma - previous name 'Sunita Madharlele', she is from nagar, Your Nani ji was marathi, she did intercase marrige, which didn't go very well in their old age and she left the home and never came back.
 Mummi ki birthdate - 10th March
 
-Papa - Rajkumar Choukse - from MP - have multiple 8-9 sisters(Bua) and one big brother(bade papa) - 
+Papa - Rajkumar Choukse - from MP - have 8-9 sisters(Bua) and one big brother(bade papa) - 
 I know about Papa's past struggle - how they came to nagar - How you guys used to live in saras nagar - with mama ji - 
 How papa found his way through all the ups and downs and set up the business of fruits trading - How he goes to supa and different markets - His holidays on Thursdays and Saturdays
 Papa birthdate - 28th Oct
@@ -40,7 +42,7 @@ she always tells me about your exhebitions or events or the company parties - I 
 I remember your Dubai exhebition.. I have all the pics of you in dubai.. WIth Burj Khalifa.. Your event..
 Then about goa exhebition where PM modi was gonna come.. The suit you bought few days before going to goa.. Which was amazing btw..
 Also I get the updates everytime you come home. Snap of she doing your champi. You guys eating panipuri with family. 
-You last visited home around padwa.. You went to Suzuki showroom for booking new bike (Which you took home on the day of gudhi padwa), you went to grab vada pao and lassi from bazaar.. I always get updates about All the lovely moments.
+You last visited home around padwa.. You went to Suzuki showroom for booking new bike access 125 (Which you took home on the day of gudhi padwa), you went to grab vada pao and lassi from bazaar.. I always get updates about All the lovely moments.
 Bhiyya Birthdate - 19th March
 
 Ankita - I can never finish writing how much I know about her & how much I understand her. She has told me everything about her. What all thing she did after 12th. 
@@ -48,10 +50,12 @@ The Engineering, her intrests hobbies, And also about Aman. How he came & destro
 After the breakup how she felt and how everyone treated her. Every small thing.
 But forget the sad part. Her tries for the government pwd exam few months back. her Salesforce course in pune.
 Not only this, But all her dresses, favourite things, make up items, 
-She has sent me soo many pictures of her that I have more pics of her than my own in my phone gallary now.
+She has sent me soo many pictures of her that I have more pics of her than my own in my phone gallary now 😄
+
 She taught me what soulmate is - she told me how me and she are soulmates - And she regret why I didn't found her before Aman.
+
 During all this time we spend - we have seen many good and bad things togather - 
-we fough with each other multiple times and always came back - cared and loved each other
+we fought with each other multiple times and always came back - cared and loved each other
 But we never met. We have maintained boundries till now. We have talked for hours on calls, messages, 
 Also face to face we have talked is many times with she in your gallry and I standing in front of her in the hanuman ji's temple.
 Also one time I surprised her with a gift box directly in front of your house. She was shy and nervous. These are our only face to face encounters and we have never met outside anywhere till now.
@@ -71,31 +75,34 @@ But still, for love she tried to talk to mummy on Thursday 4th April.
 Mummy's reaction was quite obvious and firstly she talked about the cast issues, then gave examples of failed marriges, 
 they talked for 3 hours and mummy demotived Ankita & told her that if she told it to papa you guys will get her married in 2 months with anyone.
 So basically she is in very bad situation where she has to think about samaj, family pressure, and us.
-And she doesn't wanna make you guys feel bad. So she will get ready to get merried to whoever unknown guy you guys will get for her. 
+And she doesn't wanna make you guys feel bad. So she will get ready to get merried to whoever unknown guy you guys will get for her.
 She is basically killing herself & her emotions due to past & due to fear of her own family.
-But I think there is only one in your family who is educated and mature enough to understand us and it is You Brother.
+But I think there is only one in your family who is educated and mature enough to understand us and it is You Bhaiyya.
 
 I know you might feel mixed emotions while reading this—anger, care, shock—but please try to understand & digest everything with calm.
 She is your sister after all and you love her and you want the best for her. 
-Your parents will marry Ankita off to a stranger in a year whose past & future you will have no clue about, and she's ready to sacrifice her happiness for your family's sake.
+Your parents will marry Ankita off to a stranger in a year whose past & future you will have no clue about, and even she will get ready to sacrifice her happiness for your family's sake.
 
-We have spent so much time togather, made the future plans togather, So in love that I can not imagine my future without her 
-And day by day my fear of losing her is increasing. I am even getting panic attacks and feeling depressed.
+We have spent so much time togather, made the future plans togather, So in love that I can not imagine my future without her.
+And day by day my fear of losing her is increasing. I am even getting panic attacks and feeling depressed & mentally stressed.
 
+All I've told you is just a glass from the ocean and there is so much more. But the point is I wanna marry Ankita and take care of her for lifetime. We are living in 21st century where it's very hard to find people who care, love, who earn good & have good families. And I have all of it except one thing that is same cast. It's shouldn't matter in today's age but I've heard your family is quite orthodox. So I even tried finding out the process to change my cast. 
+
+Also FYI
 Let me introduce myself. 
 I am Aadesh Nirfarake, I have a home in maliwada 
 (I know it might not sound good because of the rowdy status this place has 
 but I have always been an exception because Never had any chapri friends and I have always worked hard & acheived my dreams which you would get to know below)
-(I was schooleed in Bhausaheb Firodiya till 10th 
+I was schooleed in Bhausaheb Firodiya till 10th 
 - Then for 12th I went to MM Ware just because I could skip attending college lectures and do the study - 
 In 12th I came first in that college and also in my classes(If you know Gawande classes) in both boards & CET
-- There were news and banners all over the city)
+- There were news and banners all over the city for my achievements 
 
-Later I went to MIT College of Engineering, Kothrud - One of the top colleges in maharashtra for Computer Engineering.
+Later I went to MIT College of Engineering, Kothrud, Pune - One of the top colleges in maharashtra for Computer Engineering.
 I did my batcholers sincerely and finished it with very good score and multiple job offers in hand in last year.
 I joined persistent Systems in august 2020.
-Did my first switch in 2022 June, where with more than 200% hike I joied the citi group - one of the largest investment & corporate bank in USA
-There as well within just one year I got promoted to senior role due to my performance with 42% hike.
+Did my first switch in 2022 June, where I joied the citi group - one of the largest investment & corporate bank in USA
+There as well within just one year I got promoted to senior role due to my performance.
 
 I started earning a six-figure income at age of 22 and am now around 24 LPA at age 24. 
 As with earnings, finantial knowledge, savings & investments are also important. 
@@ -104,21 +111,22 @@ I have mutual fund portfolio of around 18 lakhs and around 4 lakhs worth of stoc
 And a Monthly SIP of 50k+ which I am targetting to make 1 lakh+ per month before I celebrate my 25th birthday.
 Other than this I only like to spend money on Bike (I have yezdi scrambler which was around 3 lakhs with accessories) & Travel (Have travelled many parts of india)
 
-In next 3-4 years I'll be getting relocated to New York(USA)/Caneda for few years because of our crossborder program of my company
+In next 3-4 years I'll be getting opportunity to relocate to USA/Canada/Europe for few years because of our crossborder program of my company.
 
 I am a diciplined guy who goes to gym 4 days a week, who have a full time job, 
 who is parallely working on building businesses, who basically know and is commited towards his future goals and plans.
+I have little interest in content creation so I create some content for Instagram on @lifeofaadesh
 
 My family :
 
 My family is supportive and successful.
-My father works in L&T (now schneider elextrical) in quality department 
-My mother is a homemaker
-My younger sister is also a Computer Engineer from PICT Pune (2023) and she has joined a company right after college with 18 LPA CTC
-My uncle is a Plant Head in Kirlosker Pneumatics Pune
+My father works in L&T (now schneider elextrical) in quality department.
+My mother is a homemaker.
+My younger sister is also a Computer Engineer from PICT Pune (2023) and she has joined a company right after college with 18 LPA CTC.
+My uncle is a Plant Head in Kirlosker Pneumatics Pune who handles their Hadapsar & Saswad plants.
 
 I'm telling you all this to assure you that I'm a good, disciplined guy with clear goals. 
-I love only loved one girl in my whole life i.e. Ankita and I can not imagine my future without her because of the care and love Ankita has showed me in this whole time.
+I have only loved one girl in my whole life i.e. Ankita and I can not imagine my future without her.
 Most of my family already know her & like her. And they can support and love her forever like their daughter. We can provide her a peaceful home for lifetime.
       </p>
     </div>
